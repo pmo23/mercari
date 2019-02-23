965ac7b2376be6aaf7cd3076ca195c223b425339
sattsu55 <owner@mbp.local>
2019-02-23 18:00:07 +0900

    basic小文字に

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,8 +21,8 @@ set :default_env, {
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
-  USERNAME: ENV["BASIC_AUTH_USER"],
-  PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+  username: ENV["BASIC_AUTH_USER"],
+  password: ENV["BASIC_AUTH_PASSWORD"]
 }
 
 set :linked_files, %w{ config/secrets.yml }
