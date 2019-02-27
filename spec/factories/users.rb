adfccbba5f681b58b8ca28e7948d35a3f489cb6b
hihihirohi <hirothugu326@gmail.com>
2019-02-27 19:19:30 +0900

    カラムの追加など

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
 
   factory :user do
+    nickname              {"サッタさん"}
+    last_name             {"サッタさん"}
+    first_name            {"サッタさん"}
+    last_name_kana        {"サッタさん"}
+    first_name_kana       {"サッタさん"}
+    date_of_birth         {"00000000"}
     email                 {"kkk@gmail.com"}
     password              {"00000000"}
     password_confirmation {"00000000"}
