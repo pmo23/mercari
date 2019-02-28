fe4bcd5e540d6482f293c6faf9726aa4e9777058
sattsu55 <owner@mbp.local>
2019-02-28 19:08:03 +0900

    ハッシュモデル作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,9 @@ class ProductsController < ApplicationController
   def new
     @prefecture = Prefecture.all
     @category = Category.all
+    @condition = Condition.all
+    @shippingpay = Shippingpay.all
+    @shippingday = Shippingday.all
   end
 
   def show
