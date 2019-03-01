200f415e62aa59c81c073bab8dd30886746a0b5b
sattsu55 <owner@mbp.local>
2019-03-01 16:16:48 +0900

    activehashの未選択項目削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,17 @@
 class Product < ApplicationRecord
 
-  has_many :product_images
+  # has_many :product_images
 
-  validates :name, presence: true, length: {maximum: 40}
-  validates :product_description, presence: true, length: {maximum: 1000}
-  validates :category1, presence: true
-  validates :category2, presence: true
-  validates :category3, presence: true
-  validates :brand, presence: true
-  validates :condition, presence: true
-  validates :postage, presence: true
-  validates :shipping_method, presence: true
-  validates :ship_from, presence: true
-  validates :shipping_date, presence: true
-  validates :price, presence: true
+  # validates :name, presence: true, length: {maximum: 40}
+  # validates :product_description, presence: true, length: {maximum: 1000}
+  # validates :category1, presence: true
+  # validates :category2, presence: true
+  # validates :category3, presence: true
+  # validates :brand, presence: true
+  # validates :condition, presence: true
+  # validates :postage, presence: true
+  # validates :shipping_method, presence: true
+  # validates :ship_from, presence: true
+  # validates :shipping_date, presence: true
+  # validates :price, presence: true
 end
