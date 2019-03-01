801de5f64f3fc667ee1bca52d6a90d87cb570832
sattsu55 <owner@mbp.local>
2019-03-01 16:17:16 +0900

    Product create編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
     @prefecture = Prefecture.all
     @category = Category.all
     @condition = Condition.all
@@ -11,5 +12,16 @@ class ProductsController < ApplicationController
   end
 
   def show
+  end
+
+  def create
+    Product.create(product_params)
+    render :index
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:product_description,:category1,:category2,:category3,:brand,:postage,:price,:condition,:shipping_method,:ship_from,:shipping_date)
   end
 end
