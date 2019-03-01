c6c409cb6505cff22f620940db966a45300134fe
sattsu55 <owner@mbp.local>
2019-03-01 16:16:23 +0900

    DB null false  設定

--- db/migrate/20190228081525_create_products.rb
+++ db/migrate/20190228081525_create_products.rb
@@ -4,13 +4,13 @@ class CreateProducts < ActiveRecord::Migration[5.0]
       t.string  :name,                 null: false
       t.text    :product_description,  null: false
       t.string  :category1,            null: false
-      t.string  :category2,            null: false
-      t.string  :category3,            null: false
-      t.string  :brand,                null: false
+      t.string  :category2
+      t.string  :category3
+      t.string  :brand
       t.string  :size
       t.string  :condition,            null: false
       t.string  :postage,              null: false
-      t.string  :shipping_method,      null: false
+      t.string  :shipping_method
       t.string  :ship_from,            null: false
       t.string  :shipping_date,        null: false
       t.integer :price,                null: false
--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,13 @@ ActiveRecord::Schema.define(version: 20190228083744) do
     t.string   "name",                              null: false
     t.text     "product_description", limit: 65535, null: false
     t.string   "category1",                         null: false
-    t.string   "category2",                         null: false
-    t.string   "category3",                         null: false
-    t.string   "brand",                             null: false
+    t.string   "category2"
+    t.string   "category3"
+    t.string   "brand"
     t.string   "size"
     t.string   "condition",                         null: false
     t.string   "postage",                           null: false
-    t.string   "shipping_method",                   null: false
+    t.string   "shipping_method"
     t.string   "ship_from",                         null: false
     t.string   "shipping_date",                     null: false
     t.integer  "price",                             null: false
