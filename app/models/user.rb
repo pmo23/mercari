9eb301e69f4eb704df794e044cf6a4f0aadd9692
hihihirohi <hirothugu326@gmail.com>
2019-03-03 16:31:39 +0900

    facebook,google認証
    userテーブルに追加させた
    新しいモデルは作成しておりません。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,32 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth, presence: true
+         :recoverable, :rememberable, :validatable, :omniauthable
+  # validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth, presence: true
+
+   def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        nickname: User.dummy_nickname(auth),
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.info.email}"
+  end
+
+  def self.dummy_nickname(auth)
+    "#{auth.info.name}"
+  end
 end
