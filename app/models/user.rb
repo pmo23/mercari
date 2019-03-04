8f4a6ade8cd52dc79b8aa9f233471c33e17ed838
sattsu55 <owner@mbp.local>
2019-03-04 16:51:56 +0900

    ユーザー登録時のバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable, :omniauthable
    has_many :products
   validates :nickname, presence: true
- 
-  validates :last_name, format: { with:/\A[ぁ-んァ-ン一-龥]\Z/, message: "名 に数字や特殊文字は使用できません"}, presence: true, unless: :uid?
-  validates :first_name, format: { with:/\A[ぁ-んァ-ン一-龥]\Z/, message: "姓 に数字や特殊文字は使用できません"}, presence: true, unless: :uid?
+
+  validates :last_name, format: { with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "名 に数字や特殊文字は使用できません"}, presence: true, unless: :uid?
+  validates :first_name, format: { with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "姓 に数字や特殊文字は使用できません"}, presence: true, unless: :uid?
   validates :last_name_kana, format: { with:/\A[ァ-ンー－]+\Z/, message: "名カナ を入力してください"}, presence: true, unless: :uid?
   validates :first_name_kana,format: { with:/\A[ァ-ンー－]+\Z/, message: "姓カナ を入力してください"}, presence: true, unless: :uid?
   validates :date_of_birth,format: { with:/\A\d{8}\z/, message: "生年月日8桁を数字で入力してください"},numericality: { greater_than: 19000000, message:"何歳ですか？笑" }, presence: true, unless: :uid?
