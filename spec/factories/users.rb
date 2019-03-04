051987fe459d9a92ac45a913b95d53b3804d00a1
sattsu55 <owner@mbp.local>
2019-03-04 21:39:41 +0900

    factorieの値変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"サッタさん"}
-    last_name             {"サッタさん"}
-    first_name            {"サッタさん"}
-    last_name_kana        {"サッタさん"}
-    first_name_kana       {"サッタさん"}
-    date_of_birth         {"00000000"}
+    nickname              {"コラッタ"}
+    last_name             {"こら"}
+    first_name            {"った"}
+    last_name_kana        {"コラ"}
+    first_name_kana       {"ッタ"}
+    date_of_birth         {"19990101"}
     email                 {"kkk@gmail.com"}
     password              {"00000000"}
     password_confirmation {"00000000"}
