45a4ff7771ee47ea88ecf0cc946a261b95c1f7ed
pmo23 <peaceminusone23@yahoo.co.jp>
2019-03-04 15:50:09 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth, presence: true
+  has_many :products
 end
