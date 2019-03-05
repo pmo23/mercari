f10187ea09f9ae23428603f3d6f178332ed8cdc7
Takahiro-Fujino <fujinon24@fujinon24noMacBook-Pro.local>
2019-03-05 16:56:51 +0900

    view、routes、controller作成

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,25 @@
+class CardsController < ApplicationController
+
+  def index
+    @user = User.new
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def set_time
+    @month = Month.all
+    @year = Year.all
+  end
+
+end
