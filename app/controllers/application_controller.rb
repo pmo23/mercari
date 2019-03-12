cb4eee03531ccbb2babd60185199b23bb7cf618b
sattsu55 <owner@mbp.local>
2019-03-12 21:09:18 +0900

    Appコントローラーからproductコントローラーに異動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :search_product
 
   private
-
-  def search_product
-    @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true)
-  end
 
   def production?
     Rails.env.production?
