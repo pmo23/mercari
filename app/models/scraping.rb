e6ed9ad9e3d45aa867d91c84d141db88413f8a83
sattsu55 <owner@mbp.local>
2019-03-13 15:59:37 +0900

    scraping完成

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -17,12 +17,41 @@ class Scraping
     agent = Mechanize.new
     page = agent.get("https://www.mercari.com/jp/category/")
     element = page.search(".category-list-individual-box-inner-box h4")
-    element.each do |ele|
+    id = 0
+    element.each.with_index(1) do |ele,i|
       # 検証用
       # puts ele.inner_text
 
       # 保存用
-      product = Category2.new(name: ele.inner_text)
+      case i
+      when 1..19
+        id = 1
+      when 20..33
+        id = 2
+      when 34..48
+        id = 3
+      when 49..61
+        id = 4
+      when 62..68
+        id = 5
+      when 69..79
+        id = 6
+      when 80..90
+        id = 7
+      when 91..100
+        id = 8
+      when 101..112
+        id = 9
+      when 113..121
+        id = 10
+      when 121..129
+        id = 11
+      when 130..136
+        id = 12
+      when 137..145
+        id = 13
+      end
+      product = Category2.new(name: ele.inner_text,category1s_id: id)
       product.save
     end
   end
@@ -41,6 +70,7 @@ class Scraping
       #   puts e
       #   puts id
       # end
+
       # 保存用
       e = ele.inner_text.strip!
       if e == "すべて"
