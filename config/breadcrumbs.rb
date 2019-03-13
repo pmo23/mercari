f1bba97ca79961147c7bda31cba39c3816cb5247
hihihirohi <hirothugu326@gmail.com>
2019-03-13 15:02:36 +0900

    パンくずリスト

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -3,11 +3,32 @@ crumb :root do
 end
 
 crumb :user do |user|
-  link "マイページ", users_path(user)
+  link "マイページ", user_path(current_user)
   parent :root
 end
 
+crumb :list do |list|
+  link "出品したやつ"
+  parent :user
+end
+
+crumb :confirmation do |confirmation|
+  link "本人情報"
+  parent :user
+end
+
+crumb :profile do |profile|
+  link "プロフィール"
+  parent :user
+end
+
+crumb :card do |card|
+  link "支払い方法"
+  parent :user
+end
+
 crumb :product do |product|
-  link "カテゴリ一覧", products_path(product)
+  link "商品詳細", products_path(product)
   parent :root
 end
+
