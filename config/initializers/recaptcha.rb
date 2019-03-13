932789254c4b85c54ca6683e7e4873a1ede6b850
sattsu55 <owner@mbp.local>
2019-03-13 20:22:53 +0900

    Secret.ymlと環境変数のマッチ

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 # config/initializers/recaptcha.rb
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_SITE_KEY']
-  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
+  config.site_key  = recaptcha_site_key
+  config.secret_key = recaptcha_secret_key
 
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
