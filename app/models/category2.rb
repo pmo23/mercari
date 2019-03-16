d1ccdde0f5161877cacd9c7b47175fd3a19f438c
sattsu55 <owner@mbp.local>
2019-03-16 17:03:59 +0900

    seedup用

--- app/models/category2.rb
+++ app/models/category2.rb
@@ -1,4 +1,4 @@
 class Category2 < ApplicationRecord
-  belongs_to :category1, optional: true
-  has_many :category3s, ->{ order(:id) }
+  # belongs_to :category1, optional: true
+  # has_many :category3s, ->{ order(:id) }
 end
