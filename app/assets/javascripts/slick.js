7d45122ddc501e328ace2ab40d171dae9622a3fe
sattsu55 <owner@mbp.local>
2019-03-17 19:45:49 +0900

    slide修正

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -1,4 +1,3 @@
-$(document).on('turbolinks:load', function() {
 $(function() {
      $('.thumb-item').slick({
           infinite: true,
@@ -7,13 +6,22 @@ $(function() {
           arrows: false,
           fade: true,
           asNavFor: '.thumb-item-nav'
+
      });
      $('.thumb-item-nav').slick({
           infinite: true,
-          slidesToShow: 4,
+          slidesToShow: 5,
           slidesToScroll: 1,
           asNavFor: '.thumb-item',
           focusOnSelect: true,
      });
-});
+     $('.slideContents').slick({
+          autoplay: true,
+          autoplaySpeed: 3000,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          dots: true,
+          pauseOnHover: false
+     });
 });
